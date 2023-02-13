753f0ebdabc346c48e6f7d5a0122f1a0cf5e7ee4
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-02-13 14:40:25 +0100

    Update poopdoors_edited.lua

--- poopdoors_edited.lua
+++ poopdoors_edited.lua
@@ -1752,166 +1752,166 @@ window_anticheatbyppasses.label("credits: Renzoo#5106", 10)
 window_anticheatbyppasses.button("method 2", function()
 	confirmnotification("AC BYPASS", "Are you sure you want to bypass anticheat with method 2?", 15, function(state)
 		if state == true then
-                task.spawn()
-			FLYING = false
-			QEfly = true
-			iyflyspeed = 1
-			vehicleflyspeed = 1
-			RunService = game:GetService("RunService")
-			Players = game:GetService("Players")
-			IYMouse = game.Players.LocalPlayer:GetMouse()
-			function sFLY(vfly)
-				repeat wait() until Players.LocalPlayer and Players.LocalPlayer.Character and getRoot(Players.LocalPlayer.Character) and Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
-				repeat wait() until IYMouse
-				if flyKeyDown or flyKeyUp then flyKeyDown:Disconnect() flyKeyUp:Disconnect() end
+                	task.spawn(function()
+				FLYING = false
+				QEfly = true
+				iyflyspeed = 1
+				vehicleflyspeed = 1
+				RunService = game:GetService("RunService")
+				Players = game:GetService("Players")
+				IYMouse = game.Players.LocalPlayer:GetMouse()
+				function sFLY(vfly)
+					repeat wait() until Players.LocalPlayer and Players.LocalPlayer.Character and getRoot(Players.LocalPlayer.Character) and Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid")
+					repeat wait() until IYMouse
+					if flyKeyDown or flyKeyUp then flyKeyDown:Disconnect() flyKeyUp:Disconnect() end
 
-				local T = getRoot(Players.LocalPlayer.Character)
-				local CONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
-				local lCONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
-				local SPEED = 0
+					local T = getRoot(Players.LocalPlayer.Character)
+					local CONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+					local lCONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+					local SPEED = 0
 
-				local function FLY()
-					FLYING = true
-					local BG = Instance.new('BodyGyro')
-					local BV = Instance.new('BodyVelocity')
-					BG.P = 9e4
-					BG.Parent = T
-					BV.Parent = T
-					BG.maxTorque = Vector3.new(9e9, 9e9, 9e9)
-					BG.cframe = T.CFrame
-					BV.velocity = Vector3.new(0, 0, 0)
-					BV.maxForce = Vector3.new(9e9, 9e9, 9e9)
-					task.spawn(function()
-						repeat wait()
-							if not vfly and Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid') then
-								Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid').PlatformStand = true
+					local function FLY()
+						FLYING = true
+						local BG = Instance.new('BodyGyro')
+						local BV = Instance.new('BodyVelocity')
+						BG.P = 9e4
+						BG.Parent = T
+						BV.Parent = T
+						BG.maxTorque = Vector3.new(9e9, 9e9, 9e9)
+						BG.cframe = T.CFrame
+						BV.velocity = Vector3.new(0, 0, 0)
+						BV.maxForce = Vector3.new(9e9, 9e9, 9e9)
+						task.spawn(function()
+							repeat wait()
+								if not vfly and Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid') then
+									Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid').PlatformStand = true
+								end
+								if CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0 or CONTROL.Q + CONTROL.E ~= 0 then
+									SPEED = 50
+								elseif not (CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0 or CONTROL.Q + CONTROL.E ~= 0) and SPEED ~= 0 then
+									SPEED = 0
+								end
+								if (CONTROL.L + CONTROL.R) ~= 0 or (CONTROL.F + CONTROL.B) ~= 0 or (CONTROL.Q + CONTROL.E) ~= 0 then
+									BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (CONTROL.F + CONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(CONTROL.L + CONTROL.R, (CONTROL.F + CONTROL.B + CONTROL.Q + CONTROL.E) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+									lCONTROL = {F = CONTROL.F, B = CONTROL.B, L = CONTROL.L, R = CONTROL.R}
+								elseif (CONTROL.L + CONTROL.R) == 0 and (CONTROL.F + CONTROL.B) == 0 and (CONTROL.Q + CONTROL.E) == 0 and SPEED ~= 0 then
+									BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (lCONTROL.F + lCONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(lCONTROL.L + lCONTROL.R, (lCONTROL.F + lCONTROL.B + CONTROL.Q + CONTROL.E) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
+								else
+									BV.velocity = Vector3.new(0, 0, 0)
+								end
+								BG.cframe = workspace.CurrentCamera.CoordinateFrame
+							until not FLYING
+							CONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+							lCONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
+							SPEED = 0
+							BG:Destroy()
+							BV:Destroy()
+							if Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid') then
+								Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid').PlatformStand = false
 							end
-							if CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0 or CONTROL.Q + CONTROL.E ~= 0 then
-								SPEED = 50
-							elseif not (CONTROL.L + CONTROL.R ~= 0 or CONTROL.F + CONTROL.B ~= 0 or CONTROL.Q + CONTROL.E ~= 0) and SPEED ~= 0 then
-								SPEED = 0
-							end
-							if (CONTROL.L + CONTROL.R) ~= 0 or (CONTROL.F + CONTROL.B) ~= 0 or (CONTROL.Q + CONTROL.E) ~= 0 then
-								BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (CONTROL.F + CONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(CONTROL.L + CONTROL.R, (CONTROL.F + CONTROL.B + CONTROL.Q + CONTROL.E) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
-								lCONTROL = {F = CONTROL.F, B = CONTROL.B, L = CONTROL.L, R = CONTROL.R}
-							elseif (CONTROL.L + CONTROL.R) == 0 and (CONTROL.F + CONTROL.B) == 0 and (CONTROL.Q + CONTROL.E) == 0 and SPEED ~= 0 then
-								BV.velocity = ((workspace.CurrentCamera.CoordinateFrame.lookVector * (lCONTROL.F + lCONTROL.B)) + ((workspace.CurrentCamera.CoordinateFrame * CFrame.new(lCONTROL.L + lCONTROL.R, (lCONTROL.F + lCONTROL.B + CONTROL.Q + CONTROL.E) * 0.2, 0).p) - workspace.CurrentCamera.CoordinateFrame.p)) * SPEED
-							else
-								BV.velocity = Vector3.new(0, 0, 0)
-							end
-							BG.cframe = workspace.CurrentCamera.CoordinateFrame
-						until not FLYING
-						CONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
-						lCONTROL = {F = 0, B = 0, L = 0, R = 0, Q = 0, E = 0}
-						SPEED = 0
-						BG:Destroy()
-						BV:Destroy()
-						if Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid') then
-							Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid').PlatformStand = false
+						end)
+					end
+					flyKeyDown = IYMouse.KeyDown:Connect(function(KEY)
+						if KEY:lower() == 'w' then
+							CONTROL.F = (vfly and vehicleflyspeed or iyflyspeed)
+						elseif KEY:lower() == 's' then
+							CONTROL.B = - (vfly and vehicleflyspeed or iyflyspeed)
+						elseif KEY:lower() == 'a' then
+							CONTROL.L = - (vfly and vehicleflyspeed or iyflyspeed)
+						elseif KEY:lower() == 'd' then 
+							CONTROL.R = (vfly and vehicleflyspeed or iyflyspeed)
+						elseif QEfly and KEY:lower() == 'e' then
+							CONTROL.Q = (vfly and vehicleflyspeed or iyflyspeed)*2
+						elseif QEfly and KEY:lower() == 'q' then
+							CONTROL.E = -(vfly and vehicleflyspeed or iyflyspeed)*2
+						end
+						pcall(function() workspace.CurrentCamera.CameraType = Enum.CameraType.Track end)
+					end)
+					flyKeyUp = IYMouse.KeyUp:Connect(function(KEY)
+						if KEY:lower() == 'w' then
+							CONTROL.F = 0
+						elseif KEY:lower() == 's' then
+							CONTROL.B = 0
+						elseif KEY:lower() == 'a' then
+							CONTROL.L = 0
+						elseif KEY:lower() == 'd' then
+							CONTROL.R = 0
+						elseif KEY:lower() == 'e' then
+							CONTROL.Q = 0
+						elseif KEY:lower() == 'q' then
+							CONTROL.E = 0
 						end
 					end)
+					FLY()
 				end
-				flyKeyDown = IYMouse.KeyDown:Connect(function(KEY)
-					if KEY:lower() == 'w' then
-						CONTROL.F = (vfly and vehicleflyspeed or iyflyspeed)
-					elseif KEY:lower() == 's' then
-						CONTROL.B = - (vfly and vehicleflyspeed or iyflyspeed)
-					elseif KEY:lower() == 'a' then
-						CONTROL.L = - (vfly and vehicleflyspeed or iyflyspeed)
-					elseif KEY:lower() == 'd' then 
-						CONTROL.R = (vfly and vehicleflyspeed or iyflyspeed)
-					elseif QEfly and KEY:lower() == 'e' then
-						CONTROL.Q = (vfly and vehicleflyspeed or iyflyspeed)*2
-					elseif QEfly and KEY:lower() == 'q' then
-						CONTROL.E = -(vfly and vehicleflyspeed or iyflyspeed)*2
+
+				function NOFLY()
+					FLYING = false
+					if flyKeyDown or flyKeyUp then flyKeyDown:Disconnect() flyKeyUp:Disconnect() end
+					if Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid') then
+						Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid').PlatformStand = false
 					end
-					pcall(function() workspace.CurrentCamera.CameraType = Enum.CameraType.Track end)
-				end)
-				flyKeyUp = IYMouse.KeyUp:Connect(function(KEY)
-					if KEY:lower() == 'w' then
-						CONTROL.F = 0
-					elseif KEY:lower() == 's' then
-						CONTROL.B = 0
-					elseif KEY:lower() == 'a' then
-						CONTROL.L = 0
-					elseif KEY:lower() == 'd' then
-						CONTROL.R = 0
-					elseif KEY:lower() == 'e' then
-						CONTROL.Q = 0
-					elseif KEY:lower() == 'q' then
-						CONTROL.E = 0
+					pcall(function() workspace.CurrentCamera.CameraType = Enum.CameraType.Custom end)
+				end
+
+				function getRoot(char)
+					local rootPart = char:FindFirstChild('HumanoidRootPart') or char:FindFirstChild('Torso') or char:FindFirstChild('UpperTorso')
+					return rootPart
+				end
+				local speaker = game.Players.LocalPlayer
+				local ch = speaker.Character
+				local prt=Instance.new("Model")
+				prt.Parent = speaker.Character
+
+				local z1 = Instance.new("Part")
+				z1.Name="Torso"
+				z1.CanCollide = false
+				z1.Anchored = true
+
+				local z2 = Instance.new("Part")
+				z2.Name="Head"
+				z2.Parent = prt
+				z2.Anchored = true
+				z2.CanCollide = false
+
+				local z3 =Instance.new("Humanoid")
+				z3.Name="Humanoid"
+				z3.Parent = prt
+
+				z1.Position = Vector3.new(0,9999,0)
+				speaker.Character=prt
+				wait(3)
+				speaker.Character=ch
+				wait(3)
+				local Hum = Instance.new("Humanoid")
+				z2:Clone()
+				Hum.Parent = speaker.Character
+				--hum = Hum
+				local root = getRoot(speaker.Character)
+				for i,v in pairs(speaker.Character:GetChildren()) do
+					if v ~= root and  v.Name ~= "Humanoid" then
+						v:Destroy()
+					end
+				end
+				root.Transparency = 0
+				root.Color = Color3.new(1, 1, 1)
+
+				local invisflingStepped
+				invisflingStepped = RunService.Stepped:Connect(function()
+					if speaker.Character and getRoot(speaker.Character) then
+						getRoot(speaker.Character).CanCollide = false
+					else
+						invisflingStepped:Disconnect()
 					end
 				end)
-				FLY()
-			end
-
-			function NOFLY()
-				FLYING = false
-				if flyKeyDown or flyKeyUp then flyKeyDown:Disconnect() flyKeyUp:Disconnect() end
-				if Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid') then
-					Players.LocalPlayer.Character:FindFirstChildOfClass('Humanoid').PlatformStand = false
-				end
-				pcall(function() workspace.CurrentCamera.CameraType = Enum.CameraType.Custom end)
-			end
-
-			function getRoot(char)
-				local rootPart = char:FindFirstChild('HumanoidRootPart') or char:FindFirstChild('Torso') or char:FindFirstChild('UpperTorso')
-				return rootPart
-			end
-			local speaker = game.Players.LocalPlayer
-			local ch = speaker.Character
-			local prt=Instance.new("Model")
-			prt.Parent = speaker.Character
-
-			local z1 = Instance.new("Part")
-			z1.Name="Torso"
-			z1.CanCollide = false
-			z1.Anchored = true
-
-			local z2 = Instance.new("Part")
-			z2.Name="Head"
-			z2.Parent = prt
-			z2.Anchored = true
-			z2.CanCollide = false
-
-			local z3 =Instance.new("Humanoid")
-			z3.Name="Humanoid"
-			z3.Parent = prt
-
-			z1.Position = Vector3.new(0,9999,0)
-			speaker.Character=prt
-			wait(3)
-			speaker.Character=ch
-			wait(3)
-			local Hum = Instance.new("Humanoid")
-			z2:Clone()
-			Hum.Parent = speaker.Character
-			--hum = Hum
-			local root = getRoot(speaker.Character)
-			for i,v in pairs(speaker.Character:GetChildren()) do
-				if v ~= root and  v.Name ~= "Humanoid" then
-					v:Destroy()
-				end
-			end
-			root.Transparency = 0
-			root.Color = Color3.new(1, 1, 1)
-
-			local invisflingStepped
-			invisflingStepped = RunService.Stepped:Connect(function()
-				if speaker.Character and getRoot(speaker.Character) then
-					getRoot(speaker.Character).CanCollide = false
-				else
-					invisflingStepped:Disconnect()
-				end
+				iyflyspeed = 1.5
+				sFLY()
+				workspace.CurrentCamera.CameraSubject = root
+				game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.All, true)
+				game:GetService("StarterGui"):SetCore("ResetButtonCallback", true)
+				normalmessage("AC BYPASS", "Done!", 5)
+				flags.anticheatbypass = true
 			end)
-			iyflyspeed = 1.5
-			sFLY()
-			workspace.CurrentCamera.CameraSubject = root
-			game:GetService("StarterGui"):SetCoreGuiEnabled(Enum.CoreGuiType.All, true)
-			game:GetService("StarterGui"):SetCore("ResetButtonCallback", true)
-			normalmessage("AC BYPASS", "Done!", 5)
-			flags.anticheatbypass = true
-end)
 		end
 	end)
 end)
