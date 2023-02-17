19662c6549ee0fd46356abdaed448012b7d3ac2f
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-02-17 13:44:43 +0100

    Update joinsolo.lua

--- joinsolo.lua
+++ joinsolo.lua
@@ -1,7 +1,11 @@
 local NotificationHolder = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Module.Lua"))()
 local Notification = loadstring(game:HttpGet("https://raw.githubusercontent.com/BocusLuke/UI/main/STX/Client.Lua"))()
 function message(text)
-	spawn(function()
+	task.spawn(function()
+		local notif = Instance.new("Sound");notif.Parent = game.SoundService;notif.SoundId = "rbxassetid://4590657391";notif.Volume = 3;notif:Play();notif.Stopped:Wait();notif:Destroy()
+	end)
+	
+	task.spawn(function()
 		local msg = Instance.new("Message",workspace)
 		msg.Text = tostring(text)
 		task.wait(5)
@@ -12,6 +16,10 @@ function message(text)
 end
 
 function normalmessage(title, text, timee)
+	task.spawn(function()
+		local notif = Instance.new("Sound");notif.Parent = game.SoundService;notif.SoundId = "rbxassetid://4590657391";notif.Volume = 3;notif:Play();notif.Stopped:Wait();notif:Destroy()
+	end)
+	
 	Notification:Notify(
 		{Title = title, Description = text},
 		{OutlineColor = Color3.fromRGB(80, 80, 80),Time = timee or 5, Type = "default"}
@@ -19,14 +27,21 @@ function normalmessage(title, text, timee)
 end
 
 function confirmnotification(title, text, timee, callback)
+	task.spawn(function()
+		local notif = Instance.new("Sound");notif.Parent = game.SoundService;notif.SoundId = "rbxassetid://4590657391";notif.Volume = 3;notif:Play();notif.Stopped:Wait();notif:Destroy()
+	end)
+	
 	Notification:Notify(
 		{Title = title, Description = text},
 		{OutlineColor = Color3.fromRGB(80, 80, 80), Time = timee or 10, Type = "option"},
 		{Image = "http://www.roblox.com/asset/?id=6023426923", ImageColor = Color3.fromRGB(255, 84, 84), Callback = callback or function(state)end}
 	)
-end
+end 
 
 function warnmessage(title, text, timee)
+	task.spawn(function()
+		local notif = Instance.new("Sound");notif.Parent = game.SoundService;notif.SoundId = "rbxassetid://4590657391";notif.Volume = 5;notif:Play();notif.Stopped:Wait();notif:Destroy()
+	end)
 	Notification:Notify(
 		{Title = title, Description = text},
 		{OutlineColor = Color3.fromRGB(80, 80, 80),Time = timee or 5, Type = "image"},
