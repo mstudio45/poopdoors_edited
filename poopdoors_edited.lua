d92092aeaa9631f67870b3b8de0e501b931712b0
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-02-24 20:42:06 +0100

    Update poopdoors_edited.lua

--- poopdoors_edited.lua
+++ poopdoors_edited.lua
@@ -246,9 +246,7 @@ end)
 local Library = nil
 if mobiletoggles then
 	if isMobile == true then
-		Library = loadstring(
-                              game:HttpGet("https://raw.githubusercontent.com/mstudio45/poopdoors_edited/main/dependencies/MobileUILibrary.lua")
-                          )()
+		Library = loadstring(game:HttpGet(("https://raw.githubusercontent.com/mstudio45/poopdoors_edited/main/dependencies/MobileUILibrary.lua"),true))()
 	else
 		Library = loadstring(game:GetObjects("rbxassetid://7657867786")[1].Source)()
 	end
