26fecde85d6830b4a788b7e85f2b0a620e2e2e68
mstudio45 <69036065+mstudio45@users.noreply.github.com>
2023-03-14 19:30:06 +0100

    Update trollface.lua

--- Codes/Entities/trollface.lua
+++ Codes/Entities/trollface.lua
@@ -1,70 +1,67 @@
-return function(_, CanEntityKill)
-	local L_1_ = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors%20Entity%20Spawner/Source.lua"))()
+return function(_, CanEntityKill) -- cannot kill idk why
+	    	local L_1_ = loadstring(game:HttpGet("https://raw.githubusercontent.com/RegularVynixu/Utilities/main/Doors%20Entity%20Spawner/Source.lua"))()
 	local L_2_ = L_1_.createEntity({
-		CustomName = "Trollface", -- Custom name of your entity
-		Model = "rbxassetid://11612531225", -- Can be GitHub file or rbxassetid
-		Speed = 5000, -- Percentage, 100 = default Rush speed
-		DelayTime = 2, -- Time before starting cycles (seconds)
-		HeightOffset = 0,
-		CanKill = CanEntityKill or false,
-		KillRange = 50,
-		BreakLights = true,
+	    CustomName = "Trollface", -- Custom name of your entity
+	    Model = "https://github.com/mstudio45/poopdoors_edited/blob/main/Codes/Entities/trollface.rbxm?raw=true", -- Can be GitHub file or rbxassetid
+	    Speed = 300, -- Percentage, 100 = default Rush speed
+		MoveDelay = 2,
+        HeightOffset = 0,
+        CanKill = false,
+        KillRange = 50,
+        SpawnInFront = false,
+        ShatterLights = true,
 		BackwardsMovement = false,
 		FlickerLights = {
-			true, -- Enabled/Disabled
-			1, -- Time (seconds)
+				true, -- Enabled/Disabled
+				5, -- Time (seconds)
 		},
 		Cycles = {
-			Min = 1,
-			Max = 4,
-			WaitTime = 2,
+				Min = 1,
+				Max = 4,
+				WaitTime = 2,
 		},
 		CamShake = {
-			false, -- Enabled/Disabled
-			{
-				3.5,
-				20,
-				0.1,
-				1
-			}, -- Shake values (don't change if you don't know)
-			100, -- Shake start distance (from Entity to you)
+				true, -- Enabled/Disabled
+				{
+					3.5,
+					20,
+					0.1,
+					1
+				}, -- Shake values (don't change if you don't know)
+				100, -- Shake start distance (from Entity to you)
 		},
-		Jumpscare = {
-			true, -- Enabled/Disabled
-			{
-				Image1 = "rbxassetid://8855759668", -- Image1 url
-				Image2 = "rbxassetid://8855759668", -- Image2 url
-				Shake = false,
-				Sound1 = {
-					8389041427, -- SoundId
-					{
-						Volume = 0.5
-					}, -- Sound properties
+	    Jumpscare = {
+				true, -- Enabled/Disabled
+				{
+					Image1 = "rbxassetid://8855759668", -- Image1 url
+					Image2 = "rbxassetid://8855759668", -- Image2 url
+					Shake = false,
+					Sound1 = {
+						8389041427, -- SoundId
+						{
+							Volume = 0.5
+						}, -- Sound properties
+					},
+					Sound2 = {
+						8389041427, -- SoundId
+						{
+							Volume = 0.5
+						}, -- Sound properties
+					},
+					Flashing = {
+						true, -- Enabled
+                         Color3.fromRGB(136, 0, 255), -- Colo
+					},
+					Tease = {
+						true, -- Enabled/Disabled
+						Min = 1,
+						Max = 1,
+					},
 				},
-				Sound2 = {
-					8389041427, -- SoundId
-					{
-						Volume = 0.5
-					}, -- Sound properties
-				},
-				Flashing = {
-					false, -- Enabled/Disabled
-					Color3.fromRGB(255, 255, 255), -- Color
-				},
-				Tease = {
-					true, -- Enabled/Disabled
-					Min = 1,
-					Max = 1,
-				},
-			},
-		},
-		CustomDialog = {
-			"Tro",
-			"lol",
-			"lol",
-			"lol"
-		}, -- Custom death message
+			}
 	})
-	
-	task.spawn(function() L_1_.runEntity(L_1_) end)
+  
+	L_2_.Model.RushNew.Footsteps.Volume = 9
+
+	task.spawn(function() L_1_.runEntity(L_2_) end)
 end
